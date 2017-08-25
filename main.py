8fb36bcf0fecec09aeb264da9d00247cfa289532
dioptrique <h0910139@gmail.com>
2017-08-25 14:17:05 +0800

    debug

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import json
 import os
 from config import * 
 print "after import"
-slack = Slacker(keys["slack"])
+#slack = Slacker(keys["slack"])
+slack = Slacker("ozXAkkPyIdiy9P3zkl0tqbFe")
 print "Slacker interfaced"
 app = Flask(__name__)
 print "Flask instance created"
